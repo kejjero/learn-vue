5535e56e9d0828f303847a662345236226e51214
Peters Maxim <74023694+kejjero@users.noreply.github.com>
2022-10-18 13:58:20 +0800

    feat!: created app

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <span>
+    <button @click="addLike">Добавить лайк</button>
+    <div>{{ likes }}</div>
+  </span>
+  <span>
+    <button @click="addDislike">Добавить дизлайк</button>
+    <div>{{ dislikes }}</div>
+  </span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1
+    },
+    addDislike() {
+      this.dislikes += 1
+    }
+  }
+}
+
+
+</script>
+
+<style>
+</style>
